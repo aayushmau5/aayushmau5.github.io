704b228a9ca7f6c93d53b337e72e0affe63b5002
aayushmau5 <aayushmau5@gmail.com>
2020-07-12 14:34:14 +0530

    Used fetch instead of XMLHttpRequest

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let btn = document.querySelector('button');
+let butn = document.querySelector('button');
 let nav = document.querySelector('.sidenav');
 let close = document.querySelector('.close');
 
-btn.addEventListener('click',() => {
+butn.addEventListener('click', () => {
     nav.style.width = '250px';
     btn.style.color = 'white';
 });
 
-close.addEventListener('click',() => {
+close.addEventListener('click', () => {
     nav.style.width = '0px';
     btn.style.color = 'black';
 })
--- weather.js
+++ weather.js
@@ -1,25 +1,20 @@
-var weather_info_delhi = document.getElementById('weather_info');
-document.getElementById('btn').addEventListener('click',Weather);
-function Weather(){
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET','https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=1107429ef67f4ff90050a129c01b5219');
-    ourRequest.onload = function(){
-     var ourData = JSON.parse(ourRequest.responseText);
-     renderHTML(ourData);
-    };
-    ourRequest.send();
-    btn.classList.add("hide-me");
-}
+let weather_info_delhi = document.getElementById('weather_info');
+let btn = document.getElementById('btn');
 
-function renderHTML(data){
-   var temp = data.main.temp;
-   temp = temp - 273;
-   temp = Math.ceil(temp);
-   var feels = data.weather[0].main;
-   var visibility = data.visibility;
-   var wind_speed = data.wind.speed;
-   var pressure = data.main.pressure;
-   var humidity = data.main.humidity;
-   var string = '<p style="font-family:Amaranth;">The temperature is '+temp+'°C and feels like '+feels+'<br> Visibility '+visibility+'<br> Wind speed: '+wind_speed+ '<br> Pressure:'+pressure+'<br> Humidity:'+humidity+' </p>';
-    weather_info_delhi.insertAdjacentHTML('beforeend',string);
+btn.addEventListener('click', () => {
+    btn.style.display = 'none';
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=1107429ef67f4ff90050a129c01b5219').then((response) => response.json()).then((data) => renderHTML(data));
+});
+
+function renderHTML(data) {
+    let temp = data.main.temp;
+    temp = temp - 273;
+    temp = Math.ceil(temp);
+    let feels = data.weather[0].main;
+    let visibility = data.visibility;
+    let wind_speed = data.wind.speed;
+    let pressure = data.main.pressure;
+    let humidity = data.main.humidity;
+    let string = '<p style="font-family:Amaranth;">The temperature is ' + temp + '°C and feels like ' + feels + '<br> Visibility ' + visibility + '<br> Wind speed: ' + wind_speed + '<br> Pressure:' + pressure + '<br> Humidity:' + humidity + ' </p>';
+    weather_info_delhi.insertAdjacentHTML('beforeend', string);
 }
